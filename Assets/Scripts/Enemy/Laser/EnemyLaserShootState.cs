cc135e6a10db28c7c3a57b87fa4cfa255b6672f8
10902026 <a0983619822@gmail.com>
2024-12-03 02:17:14 +0800

    Add Exclamation to Enemy Laser

--- Assets/Scripts/Enemy/Laser/EnemyLaserShootState.cs
+++ Assets/Scripts/Enemy/Laser/EnemyLaserShootState.cs
@@ -5,7 +5,7 @@ public class EnemyLaserShootState : EnemyState
 {
     private EnemyLaser _laserEnemy;
     private Transform _laserInstance;
-    private float _laserCooldown = 5f;  // 發射冷卻時間
+    private float _laserCooldown = 5f; // 發射冷卻時間
     private bool _hasShotLaser = false;
 
     public EnemyLaserShootState(EnemyStateMachine enemyStateMachine) : base(enemyStateMachine)
@@ -17,7 +17,10 @@ public class EnemyLaserShootState : EnemyState
     {
         base.Enter();
         _hasShotLaser = false;
-        StartLaser();
+        SetStateChangeCooldown(.3f);
+        // 顯示感嘆號，並在延遲後啟動雷射
+        EnemyStateMachine.Enemy.exclamationMark.gameObject.SetActive(true);
+        EnemyStateMachine.Enemy.StartCoroutine(StartLaserWithDelay(0.3f));
         StopEnemyMovement(); // 停止敵人移動
     }
 
@@ -27,19 +30,21 @@ public class EnemyLaserShootState : EnemyState
 
         if (_hasShotLaser)
         {
-            //Debug.Log("in_hasShotLaser\n");
             // 更新雷射持續時間，交由 Laser.cs 處理
-            if (_laserInstance == null )
+            if (_laserInstance == null)
             {
-                //Debug.Log("_laserInstance != null && !_laserInstance.gameObject.activeSelf\n");
                 _hasShotLaser = false;
-                EnemyStateMachine.ChangeState(_laserEnemy.TrackPlayerState);  // 返回追蹤狀態
+                EnemyStateMachine.ChangeState(_laserEnemy.TrackPlayerState); // 返回追蹤狀態
             }
         }
     }
 
-    private void StartLaser()
+    private IEnumerator StartLaserWithDelay(float delay)
     {
+        // 等待一段時間再啟動雷射
+        yield return new WaitForSeconds(delay);
+        _laserEnemy.exclamationMark.gameObject.SetActive(false); // 隱藏感嘆號
+
         // 直接調用 Laser.cs 的邏輯來發射雷射，無需再次實例化
         _laserInstance = Object.Instantiate(_laserEnemy.laserPrefab, _laserEnemy.transform.position, _laserEnemy.transform.rotation);
         _laserInstance.SetParent(null); // 使雷射脫離敵人父物體，防止隨敵人移動
